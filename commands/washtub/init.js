74bc1ff4cd2fd36542a7345c56a1c0b7688e2905
Andrew Libby <alibby@andylibby.org>
2018-04-10 13:25:52 -0400

    Improve error handling in washtub:init command.
    Also improves output/ communication to user in general.

--- commands/washtub/init.js
+++ commands/washtub/init.js
@@ -9,19 +9,31 @@ let { ensure_app, ensure_token } = require('../../lib/cli-util')
 function * run(context, heroku) {
   let source = context.args.database || 'DATABASE_URL'
   let app = ensure_app(context)
-  let addons = yield heroku.get(`/apps/${app}/addons`)
-  let db_addon = _.find(addons, (o) => { return _.includes(o.config_vars, source) })
 
-  if( ! db_addon ) throw new Error(`Cannot find DB addon '${source}'`)
+  cli.action.start(`Initializing washtub for your database ${source}`)
 
-  console.log(`Initializing washtub for your database ${source}`)
+  co(function *() {
+    cli.action.status("Looking up addons")
+    let addons = yield heroku.get(`/apps/${app}/addons`)
+    let db_addon = _.find(addons, (o) => { return _.includes(o.config_vars, source) })
 
-  let configs = yield heroku.get(`/apps/${app}/config-vars`)
-  let token = ensure_token(configs, app)
-  let washtub_db_client = new WashtubDatabase({ auth_token: token })
-  let result = yield washtub_db_client.create(db_addon, configs[source])
+    if( ! db_addon ) throw new Error(`Cannot find DB addon '${source}'`)
 
-  console.log('Done.')
+    cli.action.status("Fetching app configs")
+
+    let configs = yield heroku.get(`/apps/${app}/config-vars`)
+    let token = ensure_token(configs, app)
+
+    cli.action.status("Finalizing")
+
+    let washtub_db_client = new WashtubDatabase({ auth_token: token })
+    let result = yield washtub_db_client.create(db_addon, configs[source])
+
+    cli.action.done('Done.')
+  }).catch((error) => {
+    cli.error(`Error fetching configs for app ${app}.  Error contents: ${error.message}`)
+    cli.exit(1)
+  })
 }
 
 module.exports = {
